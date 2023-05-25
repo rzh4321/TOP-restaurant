4a82fc74b7d786f284009d44b0f175f754a98bb6
Ricky <rzh4321@gmail.com>
2023-05-25 16:18:12 -0400

    index.js now deletes nodes if an inactive button is clicked

--- src/home.js
+++ src/home.js
@@ -1,6 +1,27 @@
 export default function load_home() {
+    let home = document.getElementById('home');
+    home.classList.add('active');
     let first_bee = document.querySelector('img.icon');
     first_bee.insertAdjacentText('afterend', "Beary's Breakfast Bar");
+    let main = document.querySelector('.main');
+    main.insertAdjacentHTML('beforeend', `    <div class="desc card">
+    <img style="position: absolute; top: -80px; height: 100px;" src="https://eckben.github.io/bearysBreakfastBar/img/honeycomb.svg" alt="">
+    <p>Beary's has the best porridge! The atmosphere and customer service make you feel like you are sitting in the middle of the woods, eating like a bear! This is exactly the kind of place that I like to return to again and again.</p>
+    <span style="align-self: flex-start; margin-left: 50px;">Goldilocks</span>
+</div>
+<div class="card">
+    <span class="heading" style="align-self: flex-start; margin-left: 50px;">Hours</span>
+    <ul>
+        <li>Sunday: 8am - 8pm</li>
+        <li>Monday: 6am - 6pm</li>
+        <li>Tuesday: 6am - 6pm</li>
+        <li>Wednesday: 6am - 6pm</li>
+        <li>Thursday: 6am - 10pm</li>
+        <li>Friday: 6am - 10pm</li>
+        <li>Saturday: 8am - 10pm</li>
+    </ul>
+</div>`);
+
 }
 
 
@@ -11,23 +32,23 @@ export default function load_home() {
 //         <img class="icon" style="position: relative; left: 10px; top: 20px; transform: rotate(-240deg);" src="https://eckben.github.io/bearysBreakfastBar/img/bee.svg" alt="">
 
 //     </div>
-//     <div class="desc card">
-//         <img style="position: absolute; top: -80px; height: 100px;" src="https://eckben.github.io/bearysBreakfastBar/img/honeycomb.svg" alt="">
-//         <p>Beary's has the best porridge! The atmosphere and customer service make you feel like you are sitting in the middle of the woods, eating like a bear! This is exactly the kind of place that I like to return to again and again.</p>
-//         <span style="align-self: flex-start; margin-left: 50px;">Goldilocks</span>
-//     </div>
-//     <div class="card">
-//         <span class="heading" style="align-self: flex-start; margin-left: 50px;">Hours</span>
-//         <ul>
-//             <li>Sunday: 8am - 8pm</li>
-//             <li>Monday: 6am - 6pm</li>
-//             <li>Tuesday: 6am - 6pm</li>
-//             <li>Wednesday: 6am - 6pm</li>
-//             <li>Thursday: 6am - 10pm</li>
-//             <li>Friday: 6am - 10pm</li>
-//             <li>Saturday: 8am - 10pm</li>
-//         </ul>
-//     </div>
+    // <div class="desc card">
+    //     <img style="position: absolute; top: -80px; height: 100px;" src="https://eckben.github.io/bearysBreakfastBar/img/honeycomb.svg" alt="">
+    //     <p>Beary's has the best porridge! The atmosphere and customer service make you feel like you are sitting in the middle of the woods, eating like a bear! This is exactly the kind of place that I like to return to again and again.</p>
+    //     <span style="align-self: flex-start; margin-left: 50px;">Goldilocks</span>
+    // </div>
+    // <div class="card">
+    //     <span class="heading" style="align-self: flex-start; margin-left: 50px;">Hours</span>
+    //     <ul>
+    //         <li>Sunday: 8am - 8pm</li>
+    //         <li>Monday: 6am - 6pm</li>
+    //         <li>Tuesday: 6am - 6pm</li>
+    //         <li>Wednesday: 6am - 6pm</li>
+    //         <li>Thursday: 6am - 10pm</li>
+    //         <li>Friday: 6am - 10pm</li>
+    //         <li>Saturday: 8am - 10pm</li>
+    //     </ul>
+    // </div>
 //     <div class="card">
 //         <span class="heading" style="align-self: flex-start; margin-left: 50px;">Location</span>
 //         123 Forest Drive, Forestville, Maine
--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import load_home from "./home.js";
 load_page();
 load_home();
 
+let main = document.querySelector('.main');
 let home = document.getElementById('home');
 let menu = document.getElementById('menu');
 let contact = document.getElementById('contact');
 home.addEventListener('click', () => {
     if (!home.classList.contains('active')) {
+        while (main.childNodes.length != 1) {
+            main.childNodes[main.childNodes.length-1].remove();
+        }
         load_home();
     }
 })
