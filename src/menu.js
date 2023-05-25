9751c96e6d1de459967e7a304e4524f0e14eba01
Ricky <rzh4321@gmail.com>
2023-05-25 17:54:36 -0400

    finished menu page

--- src/menu.js
+++ src/menu.js
@@ -6,20 +6,54 @@ export default function load_menu() {
     let main = document.querySelector('.main');
     main.insertAdjacentHTML('beforeend', `
     <div class="title">
-    <img style="position: absolute;top: 20px;right: -5px;height: 50px;" src="https://eckben.github.io/bearysBreakfastBar/img/honeycomb.svg" alt="">        Beverages
+        <img style="position: absolute;top: 20px;right: -5px;height: 50px;" src="https://eckben.github.io/bearysBreakfastBar/img/honeycomb.svg" alt="">
+        Beverages
     </div>
-<div class="card">
-    <span class="heading" style="align-self: flex-start; margin-left: 50px;">Hours</span>
-    <ul>
-        <li>Sunday: 8am - 8pm</li>
-        <li>Monday: 6am - 6pm</li>
-        <li>Tuesday: 6am - 6pm</li>
-        <li>Wednesday: 6am - 6pm</li>
-        <li>Thursday: 6am - 10pm</li>
-        <li>Friday: 6am - 10pm</li>
-        <li>Saturday: 8am - 10pm</li>
-    </ul>
-</div>`);
+    <div class="card">
+        <span class="heading" style="align-self: flex-start; margin-left: 50px;">Honey Tea</span>
+        <p>A warm, sweet tea made with the highest quality honey and a bit of lemon to start your day off right!</p>
+        <br>
+        <br>
+        <div class='price-photo' style='align-self: flex-start; margin-left: 50px;'>
+            <span class="heading">$2</span>
+            <img src="https://eckben.github.io/bearysBreakfastBar/img/honeyTea.jpg" alt="">
+        </div>
+    </div>
+    <div class="card">
+        <span class="heading" style="align-self: flex-start; margin-left: 50px;">Honey Tea</span>
+        <p>A warm, sweet tea made with the highest quality honey and a bit of lemon to start your day off right!</p>
+        <br>
+        <br>
+        <div class='price-photo' style='align-self: flex-start; margin-left: 50px;'>
+            <span class="heading">$2</span>
+            <img src="https://eckben.github.io/bearysBreakfastBar/img/honeyTea.jpg" alt="">
+        </div>
+    </div>
+    <div class="title">
+        <img style="position: absolute;top: 20px;right: -5px;height: 50px;" src="https://eckben.github.io/bearysBreakfastBar/img/honeycomb.svg" alt="">
+        Beverages
+    </div>
+    <div class="card">
+        <span class="heading" style="align-self: flex-start; margin-left: 50px;">Honey Tea</span>
+        <p>A warm, sweet tea made with the highest quality honey and a bit of lemon to start your day off right!</p>
+        <br>
+        <br>
+        <div class='price-photo' style='align-self: flex-start; margin-left: 50px;'>
+            <span class="heading">$2</span>
+            <img src="https://eckben.github.io/bearysBreakfastBar/img/honeyTea.jpg" alt="">
+        </div>
+    </div>
+    <div class="card">
+        <span class="heading" style="align-self: flex-start; margin-left: 50px;">Honey Tea</span>
+        <p>A warm, sweet tea made with the highest quality honey and a bit of lemon to start your day off right!</p>
+        <br>
+        <br>
+        <div class='price-photo' style='align-self: flex-start; margin-left: 50px;'>
+            <span class="heading">$2</span>
+            <img src="https://eckben.github.io/bearysBreakfastBar/img/honeyTea.jpg" alt="">
+        </div>
+    </div>
+    `);
 
 }
 
