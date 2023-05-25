f32c2d85ce05091f48a31a64bde262524e4ca319
Ricky <rzh4321@gmail.com>
2023-05-25 03:06:08 -0400

    load_page() now only generates structure of page. Specific content will be moved to separate modules

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import load_page from "./load_page.js";
 load_page();
 
 let home = document.getElementById('home');
+let menu = document.getElementById('menu');
+let contact = document.getElementById('contact');
 home.addEventListener('click', () => {
     if (!home.classList.contains('active')) {
-        alert('not good');
         load_page();
     }
 })
